b7af444841e4099537ea81fb7ad9ab334bb56765
龙午 <longwu@qunhemail.com>
2025-05-08 14:34:03 +0800

    Florence2ExtraModelLoader，增加入参

--- nodes.py
+++ nodes.py
@@ -197,14 +197,26 @@ class Florence2ExtraModelLoader:
                     if "modeling_florence2.py" in files:
                         paths.append(os.path.relpath(root, start=search_path))
 
-        return {"required": {"model_path": (paths,), }}
+        return {"required": {
+            "model_path": (paths,), 
+            "precision": (['fp16','bf16','fp32'],),
+            "attention": (
+                    [ 'flash_attention_2', 'sdpa', 'eager'],
+                    {
+                    "default": 'sdpa'
+                    }),
+            },
+            "optional": {
+                "lora": ("PEFTLORA",),
+            }
+        }
 
     RETURN_TYPES = ("FL2MODEL",)
     RETURN_NAMES = ("florence2_model",)
     FUNCTION = "loadmodel"
     CATEGORY = "Florence2"
 
-    def loadmodel(self, model_path):
+    def loadmodel(self, model_path, precision, attention, lora=None):
         device = mm.get_torch_device()
         for search_path in folder_paths.get_folder_paths("florence2"):
             if os.path.exists(search_path):
@@ -213,19 +225,23 @@ class Florence2ExtraModelLoader:
                     model_path = path
                     break
         print(f"Loading model from {model_path}")
-        torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
-        with patch(
-            "transformers.dynamic_module_utils.get_imports", fixed_get_imports
-        ):  # workaround for unnecessary flash_attn requirement
-            model = AutoModelForCausalLM.from_pretrained(
-                model_path,
-                device_map=device,
-                torch_dtype=torch_dtype,
-                trust_remote_code=True,
-            )
+        dtype = {"bf16": torch.bfloat16, "fp16": torch.float16, "fp32": torch.float32}[precision]
+        print(f"Loading model from {model_path}")
+        print(f"using {attention} for attention")
+        with patch("transformers.dynamic_module_utils.get_imports", fixed_get_imports): #workaround for unnecessary flash_attn requirement
+            model = AutoModelForCausalLM.from_pretrained(model_path, attn_implementation=attention, device_map=device, torch_dtype=dtype,trust_remote_code=True)
         processor = AutoProcessor.from_pretrained(model_path, trust_remote_code=True)
 
-        florence2_model = {"model": model, "processor": processor, "dtype": torch_dtype}
+        if lora is not None:
+            from peft import PeftModel
+            adapter_name = lora
+            model = PeftModel.from_pretrained(model, adapter_name, trust_remote_code=True)
+        
+        florence2_model = {
+            'model': model, 
+            'processor': processor,
+            'dtype': dtype
+            }
 
         return (florence2_model,)
     
